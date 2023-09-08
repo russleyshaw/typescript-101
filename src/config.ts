b3abcad505b5e3b07e685bccba6e99e40ff34f88
Russley Shaw <russleyshaw@gmail.com>
2023-09-07 20:34:36 -0500

    set displayname by package json

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import PACKAGE_JSON from "../package.json";
 
 export const APP_NAME = PACKAGE_JSON.name;
 export const APP_VERSION = PACKAGE_JSON.version;
+export const APP_DISPLAY_NAME = PACKAGE_JSON.displayName;
 
 export const SHOW_EULA = true;
 export const SHOW_CHANGELOG = true;
