30c0009dd4076ad3c4426edacd777fc658ec4236
Russley Shaw <russleyshaw@gmail.com>
2023-09-07 20:32:07 -0500

    Don't show EULA or Changelog.

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import PACKAGE_JSON from "../package.json";
 export const APP_NAME = PACKAGE_JSON.name;
 export const APP_VERSION = PACKAGE_JSON.version;
 
-export const SHOW_EULA = true;
-export const SHOW_CHANGELOG = true;
+export const SHOW_EULA = false;
+export const SHOW_CHANGELOG = false;
